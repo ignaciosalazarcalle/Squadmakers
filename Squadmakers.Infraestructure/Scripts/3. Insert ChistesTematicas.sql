10f81ba52b684eb5a3cc2ca99664f40944b48ed9
ignaciosalazarcalle <ignaciosalazarcalle@gmail.com>
2024-01-11 20:58:06 +0100

    Update 3. Insert ChistesTematicas.sql

--- Squadmakers.Infraestructure/Scripts/3. Insert ChistesTematicas.sql
+++ Squadmakers.Infraestructure/Scripts/3. Insert ChistesTematicas.sql
@@ -10,19 +10,27 @@ INSERT INTO [dbo].[Chistes]
     [UsuarioId]
 )
 SELECT NEWID(),
-       CONCAT('Chiste tem·tica ', Tematica.Nombre),
-       CONCAT('Chiste tem·tica ', Tematica.Nombre, ' para el Usuario ', usuario.Nombre),
-	   usuario.Nombre,
+       CONCAT('Chiste tem√°tica ', tematica.Nombre),
+       CONCAT('Chiste tem√°tica ', tematica.Nombre, ' para el Usuario ', usuario.Nombre),
+       usuario.Nombre,
        usuario.Id
 FROM dbo.Usuarios usuario
-    CROSS JOIN dbo.Tematicas Tematica;
+    CROSS APPLY
+(
+    SELECT TOP 3
+           ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS RowNum
+    FROM master.dbo.spt_values
+) AS ChistesGenerados
+    CROSS JOIN dbo.Tematicas tematica;
 
 INSERT INTO [dbo].[ChistesTematicas]
 (
     [ChisteId],
     [TematicaId]
 )
-SELECT chiste.Id,
-       Tematica.id
+SELECT chiste.Id AS ChisteId, tematica.TematicaId
 FROM dbo.Chistes chiste
-    CROSS JOIN dbo.Tematicas Tematica;
+JOIN (
+    SELECT Id TematicaId, Nombre AS TematicaNombre
+    FROM dbo.Tematicas tematica
+) tematica ON chiste.Titulo LIKE '%' + tematica.TematicaNombre + '%'
